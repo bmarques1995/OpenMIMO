e1f221ab9ff7dcab407aac9e744b69aefa16f783
bmarques1995 <brunosmarques1995@hotmail.com>
2022-05-24 11:40:09 -0300

    Added WIN32 entrypoint and edited cmake scripts to only include completely implemented ImGUI backends

--- OpenMIMOPlot/entry.cpp
+++ OpenMIMOPlot/entry.cpp
@@ -1,5 +1,23 @@
 #include "Core/Application.hpp"
 
+#ifdef WINDOWS_PLOT
+
+#include <windows.h>
+
+INT APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    Application* app = new Application();
+    app->Run();
+    delete app;
+
+    return 0;
+}
+
+#else
+
 int main(int argc, char** argv)
 {
     Application* app = new Application();
@@ -8,3 +26,5 @@ int main(int argc, char** argv)
 
     return 0;
 }
+
+#endif // WINDOWS_PLOT
--- cmake/OpenMIMOPlot.cmake
+++ cmake/OpenMIMOPlot.cmake
@@ -13,7 +13,17 @@ include(implot)
 file(GLOB_RECURSE PLOT_MIMO_SOURCES RELATIVE ${CMAKE_CURRENT_SOURCE_DIR} "OpenMIMOPlot/*.cpp")
 file(GLOB_RECURSE PLOT_MIMO_HEADERS RELATIVE ${CMAKE_CURRENT_SOURCE_DIR} "OpenMIMOPlot/*.hpp")
 
-add_executable(OpenMIMOPlot ${PLOT_MIMO_HEADERS} ${PLOT_MIMO_SOURCES})
 
+
+if(WIN32)
+    add_compile_definitions(WINDOWS_PLOT)
+    if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+        add_executable(OpenMIMOPlot WIN32 ${PLOT_MIMO_HEADERS} ${PLOT_MIMO_SOURCES})
+    else()
+        add_executable(OpenMIMOPlot ${PLOT_MIMO_HEADERS} ${PLOT_MIMO_SOURCES})
+    endif()
+else()
+    add_executable(OpenMIMOPlot ${PLOT_MIMO_HEADERS} ${PLOT_MIMO_SOURCES})
+endif()
 target_include_directories(OpenMIMOPlot PRIVATE ./OpenMIMOPlot IMPLOT_INCLUDE_DIR IMGUI_INCLUDE_DIR ./vendor/glfw/include ./vendor/glad/include)
 target_link_libraries(OpenMIMOPlot PRIVATE implot imgui glad glfw)
